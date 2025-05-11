ae4f065d941a875610895bba46541003541791ab
nicolas-loretan <nicolas.loretan@edu.vs.ch>
2025-05-11 14:07:48 +0200

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1,1 +1,10 @@
+const socket = io(); // Si les fichiers sont sur le même domaine
+
+// Émettre un message
+socket.emit("message", "Bonjour serveur !");
+
+// Écouter un message
+socket.on("message", (data) => {
+  console.log("Message du serveur :", data);
+});
 
