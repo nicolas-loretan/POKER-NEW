ec5978dbc4dc7ad2105766b936116dacf69e1f57
nicolas-loretan <nicolas.loretan@edu.vs.ch>
2025-05-13 19:46:04 +0200

    Update game.js

--- public/game.js
+++ public/game.js
@@ -8,3 +8,8 @@ socket.on("message", (data) => {
   console.log("Message du serveur :", data);
 });
 
+socket.on("h1Modif", (text) => {
+  const h1 = document.querySelector('h1');  // Sélectionne le premier <h1> trouvé
+  h1.textContent = text;
+});
+
