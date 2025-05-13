6af1296b98a0040f6f3f69ecdf99bceb924f00c0
nicolas-loretan <nicolas.loretan@edu.vs.ch>
2025-05-13 19:50:16 +0200

    Create login.js

---
+++ public/login.js
@@ -0,0 +1,18 @@
+const socket = io();
+
+  const form = document.getElementById('startForm');
+  const input = document.getElementById('playerName');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = input.value.trim();
+    socket.emit("verifierNom", name);
+  });
+
+  socket.on("nomValide", (url) => {
+    window.location.href = url; // redirection vers page personnalisée
+  });
+
+  socket.on("erreurNom", (message) => {
+    alert(message); // affiche erreur si nom invalide
+  });
