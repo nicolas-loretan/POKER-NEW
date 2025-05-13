87c2621291d24fca9ddfa8000db77dac3f26155c
nicolas-loretan <nicolas.loretan@edu.vs.ch>
2025-05-13 13:25:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 // Important : CORS configuration pour accepter les connexions front-end
 const io = new Server(server, {
   cors: {
-    origin: "/public/game.js", // Remplace '*' par l’URL de ton front pour plus de sécurité
+    origin: "*", // Remplace '*' par l’URL de ton front pour plus de sécurité
     methods: ["GET", "POST"]
   }
 });
