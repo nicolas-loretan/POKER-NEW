b30d2e7676cec19d530d78522de8076398b3fc39
nicolas-loretan <nicolas.loretan@edu.vs.ch>
2025-05-14 10:54:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ for (let key in playerById) {
   socket.on("disconnect", () => {
     console.log("❌ Déconnexion :", socket.id);
     if (userBySockets.has(socket)) {
-	let p = userBySockets[socket]
+	let p = userBySockets.get(socket);
 	p.socket = null
 	userBySockets.delete(socket);
 
